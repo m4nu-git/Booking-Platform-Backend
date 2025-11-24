71b23890c7901a2557eee3f945294eaa1b407bd0
m4nu-git <manudev912@gmail.com>
2025-11-24 18:46:39 +0530

    Added notification Service

--- NotificationService/src/server.ts
+++ NotificationService/src/server.ts
@@ -5,6 +5,8 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import { setupMailerWorker } from './processors/email.processor';
+import { NotificationDto } from './dto/notification.dto';
 import { addEmailToQueue } from './producers/email.producer';
 const app = express();
 
@@ -16,7 +18,7 @@ app.use(express.json());
 
 app.use(attachCorrelationIdMiddleware);
 app.use('/api/v1', v1Router);
-app.use('/api/v2', v2Router);
+app.use('/api/v2', v2Router); 
 
 
 /**
@@ -30,16 +32,18 @@ app.use(genericErrorHandler);
 app.listen(serverConfig.PORT, () => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
     logger.info(`Press Ctrl+C to stop the server.`);
+    setupMailerWorker();
+    logger.info(`Mailer worker setup completed!`);
 
-    for (let i = 0; i < 10; i++) {
-        addEmailToQueue({
-            to: `sample from booking ${1}`,
-            subject: "Sample Email booking",
-            templateId: "sample-template",
-            params: {
-                name: "John Doe",
-                orderId: "12345",
-            }
-        })
+    const sampleNotification: NotificationDto = {
+        to: "sample",
+        subject: "Sample Email",
+        templateId: "sample-template",
+        params: {
+            name: "John Doe",
+            orderId: "12345",
+        }
     }
+
+    addEmailToQueue(sampleNotification);
 });
