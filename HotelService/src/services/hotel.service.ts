a19c32b97ca8e581ebe397626c6f0df287f7b213
m4nu-git <manudev912@gmail.com>
2026-02-09 10:14:40 +0530

    Added Base repository for common repo functions

--- HotelService/src/services/hotel.service.ts
+++ HotelService/src/services/hotel.service.ts
@@ -1,36 +1,26 @@
 import { createHotelDTO } from "../dto/hotel.dto";
-import {
-  createHotel,
-  getAllHotels,
-  getHotelById,
-  softDeleteHotel,
-  updateHotel,
-} from "../repositories/hotel.repository";
+import { HotelRepository } from "../repositories/hotel.repository";
+
+
+const hotelRepository = new HotelRepository();
+
 
 export async function createHotelService(hotelData: createHotelDTO) {
-  const hotel = await createHotel(hotelData);
-  return hotel;
+    const hotel = await hotelRepository.create(hotelData);
+    return hotel;
 }
 
 export async function getHotelByIdService(id: number) {
-  const hotel = await getHotelById(id);
-  return hotel;
+    const hotel = await hotelRepository.findById(id);
+    return hotel;
 }
 
 export async function getAllHotelsService() {
-  const hotels = await getAllHotels();
-  return hotels;
+    const hotels = await hotelRepository.findAll();
+    return hotels;
 }
 
 export async function deleteHotelService(id: number) {
-  const response = await softDeleteHotel(id);
-  return response;
-}
-
-export async function updateHotelService(
-  id: number,
-  hotelData: createHotelDTO
-) {
-  const hotel = await updateHotel(id, hotelData);
-  return hotel;
+    const response = await hotelRepository.softDelete(id);
+    return response;
 }
